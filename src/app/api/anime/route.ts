6213a7027b6bb2f793842f7f49c39ea54478e2a2
Vedant bhavsar <vedantbhavsar.a10@gmail.com>
2025-01-12 14:08:19 +0530

    add adstxt

--- src/app/api/anime/route.ts
+++ src/app/api/anime/route.ts
@@ -1,8 +1,13 @@
+import { hianime } from '@/lib/animeClient'
 import { NextResponse } from 'next/server'
 
 export async function GET() {
   try {
-    return NextResponse.json('server is working!!')
+    const anime = await hianime.search('jujutsu kaisen')
+    const series = await hianime.getInfo('jujutsu-kaisen-0-movie-17763')
+    const a = await hianime.getEpisodes('jujutsu-kaisen-tv-534')
+    const b = await hianime.getEpisodeSources('jujutsu-kaisen-tv-534?ep=10789')
+    return NextResponse.json({ anime, series, a, b })
   } catch (error: any) {
     console.log(error)
     return NextResponse.json(error)
